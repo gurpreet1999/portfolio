a640c7f6da1727726692b4d1166f30d3edab00bb
Gurpreet Singh <127664291+gurpreet1999@users.noreply.github.com>
2025-08-04 19:02:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ module "vpc" {
 }
 
 module "security_group" {
-  source = "./modules/sg"
+  source = "./modules/SG"
   vpc_id = module.vpc.vpc_id   
 }
 
@@ -25,3 +25,4 @@ module "ec2" {
   subnet_id         = module.vpc.subnet_id      
   security_group_id = module.security_group.webserversg_id 
 }
+
