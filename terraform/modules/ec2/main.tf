f3bcb98def1624644cbb3e92533dbfc544be8184
gurpreet1999 <kinggurpreet28@gmail.com>
2025-08-05 05:58:41 +0000

    ok

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "webserver" {
   subnet_id  = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
-  key_name = "practice"
+  key_name = "ok"
   tags = {
     Name = "webserver"
   }
