7c2e48ae7c5a9c007b766ae91430c643cdc86272
gurpreet1999 <kinggurpreet28@gmail.com>
2025-08-05 06:24:10 +0000

    ok

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -9,12 +9,23 @@ resource "aws_security_group" "webserversg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+   from_port =22
+   to_port =22
+   protocol ="ssh"
+   cidr_blocks=["0.0.0.0/0"]
+
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+
+
 
   tags = {
     Name = "webserver-sg"
